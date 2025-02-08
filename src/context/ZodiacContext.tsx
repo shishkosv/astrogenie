7d599e02aa5d0f0b03041348588614a221b2d5d8
sergeymilashico <sergey@milashico.com>
2025-02-07 20:18:24 -0600

    make zodiaccontext use zodiacsignservice

--- src/context/ZodiacContext.tsx
+++ src/context/ZodiacContext.tsx
@@ -1,4 +1,7 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { zodiacSignService } from '../services/zodiacSignService';
+import { zodiacSigns } from '../services/zodiacSigns';
+import type { ZodiacSignInfo } from '../types/ZodiacSignInfo';
 
 export type ZodiacSign = {
   name: string;
@@ -9,15 +12,40 @@ export type ZodiacSign = {
 type ZodiacContextType = {
   selectedSign: ZodiacSign | null;
   setSelectedSign: (sign: ZodiacSign) => void;
+  current: () => ZodiacSignInfo | null;
 };
 
 const ZodiacContext = createContext<ZodiacContextType | undefined>(undefined);
 
 export const ZodiacProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [selectedSign, setSelectedSign] = useState<ZodiacSign | null>(null);
+  const [currentInfo, setCurrentInfo] = useState<ZodiacSignInfo | null>(null);
+
+  // Preselect first zodiac sign
+  useEffect(() => {
+    if (!selectedSign && zodiacSigns.length > 0) {
+      setSelectedSign({
+        name: zodiacSigns[0].name,
+        dateRange: zodiacSigns[0].dates,
+        period: ''
+      });
+    }
+  }, []);
+
+  const handleSetSelectedSign = (sign: ZodiacSign) => {
+    setSelectedSign(sign);
+    const signInfo = zodiacSignService.Current(sign.name);
+    setCurrentInfo(signInfo);
+  };
+
+  const current = () => currentInfo;
 
   return (
-    <ZodiacContext.Provider value={{ selectedSign, setSelectedSign }}>
+    <ZodiacContext.Provider value={{ 
+      selectedSign, 
+      setSelectedSign: handleSetSelectedSign,
+      current 
+    }}>
       {children}
     </ZodiacContext.Provider>
   );
